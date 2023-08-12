11d5f279692f06b5860d1dc47b52a97e29786656
washingtoncaraujo <111454028+washingtoncaraujo@users.noreply.github.com>
2023-08-12 15:44:05 -0300

    comentarios

--- LabSemana1/Lab1Semana1.js
+++ LabSemana1/Lab1Semana1.js
@@ -1,14 +1,16 @@
+// Criação da clasee Pedido
 class Pedido{
     constructor(id, valor, status){
         this.id = id;
         this.valor = valor;
         this.status = status
     }
-
 }
 
+// Variável global com a lista dos pedidos
 var listaDePedidos = [];
 
+// Função para adicionar pedidos
 let adicionarPedido = (id, valor, status) => {
     let pedido = new Pedido;
     pedido.id = id;
@@ -17,16 +19,19 @@ let adicionarPedido = (id, valor, status) => {
     listaDePedidos.push(pedido);
 } 
 
+// Função para lista pedidos por status
 let listarPedidosStatus = (situacao) => {
     let listaPedStat = listaDePedidos.filter((pedido) => pedido.status === situacao);
     console.log(listaPedStat);
 }
 
+// Função para atualizar pedido
 const atualizarStatus = (id, newStatus) => {
     let listaStatus = listaDePedidos.findIndex((pedido) => pedido.id === id);
     listaDePedidos[listaStatus].status = newStatus;
 }
 
+// Função para calcular total de pedidos
 let calcularTotalPedidos = () => {
     let t = listaDePedidos.length;
     let valor;
@@ -38,21 +43,24 @@ let calcularTotalPedidos = () => {
     return total;
 }
 // teste do código
-
+// Inserindo pedidos
 adicionarPedido(1, 100.00, "Aguardando Pagamento");
 adicionarPedido(2, 150.00, "Enviado");
 adicionarPedido(3, 80.00, "Entregue");
 adicionarPedido(4, 200.00, "Aguardando Pagamento");
 
+// listando pedidos por status
 console.log("Lista pedidos por Status: ");
 listarPedidosStatus("Aguardando Pagamento");
 
+// Atualizando status de pedido
 console.log("Lista de pedidos original: ");
 console.log(listaDePedidos);
 atualizarStatus(3, "Pago");
 console.log("Lista de pedidos atualizada: ");
 console.log(listaDePedidos);
 
+// Calculando total de pedidos
 console.log(`Total de pedidos: R$ ${calcularTotalPedidos()}`);
 
 
