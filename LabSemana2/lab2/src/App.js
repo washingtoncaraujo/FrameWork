6995de8a89eeadb38e32e1c4f251cdfe74321be3
washingtoncaraujo <111454028+washingtoncaraujo@users.noreply.github.com>
2023-08-21 15:00:10 -0300

    Criaçãao projeto LabSemana2

---
+++ LabSemana2/lab2/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Subtitle from './Subtitle';
+
+function App() {
+  return (
+    <div className='App'>
+      <h1>Título Simples</h1>
+      <Subtitle text = 'Meu subtítulo'/>
+    </div>
+  );
+}
+
+export default App;
---
+++ LabSemana2/lab2/src/Subtitle.js
@@ -0,0 +1,9 @@
+import React from 'react';
+// import './Subtitle.css';
+
+
+function Subtitle(props) {
+  return <h2>{props.text}</h2>
+}
+
+export default Subtitle;
