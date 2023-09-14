0e03589c7f3c2ce12133dec3388c4391fcd4b19b
washingtoncaraujo <111454028+washingtoncaraujo@users.noreply.github.com>
2023-09-14 18:07:06 -0300

    editado url da api

--- API/Axios/ReactApi/react-gamecollection/src/config.js
+++ API/Axios/ReactApi/react-gamecollection/src/config.js
@@ -1,7 +1,7 @@
 // Arquivo de configuração para a URL da API
 
 // Define a URL da API que será usada no projeto. Certifique-se de substituir esta URL pela URL real da sua API.
-const API_URL = "https://6501fdd2736d26322f5c9a87.mockapi.io/api/";
+const API_URL = "https://650250ed736d26322f5ced53.mockapi.io/api/";
 
 // Exporta a URL da API como padrão, para que ela possa ser importada em outros arquivos.
 export default API_URL;
