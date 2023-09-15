52a4756d5ae02ff42fdaef3f4a06e5661bff1a03
washingtoncaraujo <111454028+washingtoncaraujo@users.noreply.github.com>
2023-09-15 19:05:46 -0300

    Projeto lab Semana 5

---
+++ LabSemana5/ControleViagens/src/App.js
@@ -0,0 +1,96 @@
+// Importações necessárias do React e de outras bibliotecas
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import ViageForm from "./components/ViageForm";
+import ViageTable from "./components/ViageTable";
+import {
+  CssBaseline,
+  Container,
+  Typography,
+  AppBar,
+  Toolbar
+} from "@mui/material";
+import API_URL from "./config";
+
+// Estilos CSS para elementos específicos do componente
+const appBarStyle = {
+  marginBottom: "20px"
+};
+
+const pageTitleStyle = {
+  fontSize: "2rem",
+  fontWeight: "bold",
+  marginBottom: "20px"
+};
+
+// Componente principal da aplicação
+function App() {
+  // Estados locais para armazenar a lista de jogos e controlar a exibição do formulário
+  const [viages, setviages] = useState([]);
+  const [showForm, setShowForm] = useState(false);
+
+  // Efeito colateral para buscar a lista de jogos ao montar o componente
+  useEffect(() => {
+    fetchviages();
+  }, []);
+
+  // Função assíncrona para buscar a lista de jogos da API
+  const fetchviages = async () => {
+    try {
+      const response = await axios.get(`${API_URL}/Viagens`);
+      setviages(response.data);
+    } catch (error) {
+      console.error("Erro ao buscar viagens:", error);
+    }
+  };
+
+  // Função para adicionar um novo jogo à coleção
+  const handleAddViage = async (newViage) => {
+    try {
+      await axios.post(`${API_URL}/Viagens`, newViage);
+      fetchviages(); // Atualiza a lista de jogos após a adição
+      setShowForm(false); // Fecha o formulário de adição
+    } catch (error) {
+      console.error("Erro ao adicionar viagem:", error);
+    }
+  };
+
+  // Função para excluir um jogo da coleção
+  const handleDeleteViage = async (viageId) => {
+    try {
+      await axios.delete(`${API_URL}/Viagens/${viageId}`);
+      fetchviages(); // Atualiza a lista de jogos após a exclusão
+    } catch (error) {
+      console.error("Erro ao excluir viagem:", error);
+    }
+  };
+
+  // Renderização do componente
+  return (
+    <div>
+      <CssBaseline />
+      <AppBar position="static" style={appBarStyle}>
+        <Toolbar>
+          <Typography variant="h6">Viagens</Typography>
+        </Toolbar>
+      </AppBar>
+      <Container maxWidth="lg">
+        <Typography variant="h2" align="center" style={pageTitleStyle}>
+        Viagens Programadas
+        </Typography>
+        {/* Condicional para renderizar o formulário ou a tabela de viagens */}
+        {showForm ? (
+          <ViageForm handleAddViage={handleAddViage} setShowForm={setShowForm} />
+        ) : (
+          <ViageTable
+            viage={viages}
+            handleDeleteViage={handleDeleteViage}
+            setShowForm={setShowForm}
+          />
+        )}
+      </Container>
+    </div>
+  );
+}
+
+export default App; // Exportação do componente principal
---
+++ LabSemana5/ControleViagens/src/config.js
@@ -0,0 +1,7 @@
+// Arquivo de configuração para a URL da API
+
+// Define a URL da API que será usada no projeto. Certifique-se de substituir esta URL pela URL real da sua API.
+const API_URL = "https://650250ed736d26322f5ced53.mockapi.io/api";
+
+// Exporta a URL da API como padrão, para que ela possa ser importada em outros arquivos.
+export default API_URL;
