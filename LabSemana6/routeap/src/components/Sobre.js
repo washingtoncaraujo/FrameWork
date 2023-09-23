e63352b6b93bf4b7c015f0937fd0ce5752bf6e42
washingtoncaraujo <111454028+washingtoncaraujo@users.noreply.github.com>
2023-09-23 18:27:30 -0300

    rota redame finalizada

--- LabSemana6/routeap/src/components/Sobre.js
+++ LabSemana6/routeap/src/components/Sobre.js
@@ -1,25 +1,40 @@
 import React from "react";
 import { TextField, Button, Grid, Paper, Typography, Container } from "@mui/material";
-import {Routers, Route} from 'react-router-dom';
-
+import {BrowserRouter as Router, Routes, Link, Route} from 'react-router-dom';
+import Readme from '../rotas/readme';
 function Sobre({setShowForm}) {
   const buttonStyle = {
     marginRight: "18px"
   };
-
-
-  return (
-    <div>
-      <Route>
-        <Route path="/readme" element = {<Readme />}></Route>
-      </Route>
-      <Paper>
-      <Typography variant="h6" gutterBottom>
-        Dúvidas:
-      </Typography>
-      <p>WCA</p>
-      </Paper>
+return (    
+  <div>
       
+    <Paper>
+      <Router>
+      <div>
+        <header>
+          <h1>Dúvidas</h1>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/readme">Manual</Link>
+              </li>
+              <li>
+                <Link to="/about">Sobre</Link>
+              </li>
+              <li>
+                <Link to="/users/123">Perfil do Usuário</Link>
+              </li>
+            </ul>
+          </nav>
+        </header>
+        <Routes>
+          <Route path="/readme" element={<Readme />} />
+          {/* Adicione outras rotas conforme necessário */}
+        </Routes>
+      </div>
+      </Router>
+      </Paper>      
       
       <Button onClick={() => setShowForm(3)} style={buttonStyle}>Voltar
      </Button>
