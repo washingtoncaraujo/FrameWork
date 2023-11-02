fb618128ca3fd16f91300c573547b2339d2b9c4e
washingtoncaraujo <111454028+washingtoncaraujo@users.noreply.github.com>
2023-11-02 10:39:53 -0300

    Reiniciado projeto semana3 desafio

---
+++ ModuloII/Semana3/DesafioSemana3/bd_semana3/src/main/java/com/bddev/bd_semana3/BdSemana3Application.java
@@ -0,0 +1,13 @@
+package com.bddev.bd_semana3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BdSemana3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BdSemana3Application.class, args);
+	}
+
+}
---
+++ ModuloII/Semana3/DesafioSemana3/bd_semana3/src/test/java/com/bddev/bd_semana3/BdSemana3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bddev.bd_semana3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BdSemana3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
