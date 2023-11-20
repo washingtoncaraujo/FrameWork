b16ad099b9cb99337944f077a5dd06593ec82028
washingtoncaraujo <111454028+washingtoncaraujo@users.noreply.github.com>
2023-11-20 10:32:53 -0300

    projeto semana 4 finalizado

---
+++ ModuloII/Semana4/SpringJWT/springjwt/src/main/java/dev/br/springjwt/repositories/UserRepository.java
@@ -0,0 +1,15 @@
+package dev.br.springjwt.repositories;
+
+
+import dev.br.springjwt.model.User;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class UserRepository {
+    public User findUserByEmail(String email){
+        User user = new User(email,"123456");
+        user.setFirstName("FirstName");
+        user.setLastName("LastName");
+        return user;
+    }
+}
