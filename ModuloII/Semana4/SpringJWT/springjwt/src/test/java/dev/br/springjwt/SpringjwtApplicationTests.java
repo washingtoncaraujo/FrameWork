1bdb67cd66a999f0a8264fb37680f48ce3cd4779
washingtoncaraujo <111454028+washingtoncaraujo@users.noreply.github.com>
2023-11-20 09:44:52 -0300

    Iniciado projeto semana 4

---
+++ ModuloII/Semana4/SpringJWT/springjwt/src/test/java/dev/br/springjwt/SpringjwtApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.br.springjwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringjwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
